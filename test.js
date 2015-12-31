85a4aaa0d9ebb91be2f7a7c608c0e03c93b20afe
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 18:58:59 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,50 +1,43 @@
 'use strict';
-var path = require('path');
 var test = require('ava');
 var requireUncached = require('require-uncached');
 
 test('.data', function (t) {
 	process.env.XDG_DATA_HOME = 'data';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.data === 'data');
-	t.end();
+	t.is(xdgBasedir.data, 'data');
 });
 
 test('.config', function (t) {
 	process.env.XDG_CONFIG_HOME = 'config';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.config === 'config');
-	t.end();
+	t.is(xdgBasedir.config, 'config');
 });
 
 test('.cache', function (t) {
 	process.env.XDG_CACHE_HOME = 'cache';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.cache === 'cache');
-	t.end();
+	t.is(xdgBasedir.cache, 'cache');
 });
 
 test('.runtime', function (t) {
 	process.env.XDG_RUNTIME_DIR = 'runtime';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.runtime === 'runtime');
-	t.end();
+	t.is(xdgBasedir.runtime, 'runtime');
 });
 
 test('.dataDirs', function (t) {
 	process.env.XDG_DATA_DIRS = 'dirs:data_dirs';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.dataDirs[0] === 'data');
-	t.assert(xdgBasedir.dataDirs[1] === 'dirs');
-	t.assert(xdgBasedir.dataDirs[2] === 'data_dirs');
-	t.end();
+	t.is(xdgBasedir.dataDirs[0], 'data');
+	t.is(xdgBasedir.dataDirs[1], 'dirs');
+	t.is(xdgBasedir.dataDirs[2], 'data_dirs');
 });
 
 test('.configDirs', function (t) {
 	process.env.XDG_CONFIG_DIRS = 'dirs:config_dirs';
 	var xdgBasedir = requireUncached('./');
-	t.assert(xdgBasedir.configDirs[0] === 'config');
-	t.assert(xdgBasedir.configDirs[1] === 'dirs');
-	t.assert(xdgBasedir.configDirs[2] === 'config_dirs');
-	t.end();
+	t.is(xdgBasedir.configDirs[0], 'config');
+	t.is(xdgBasedir.configDirs[1], 'dirs');
+	t.is(xdgBasedir.configDirs[2], 'config_dirs');
 });
